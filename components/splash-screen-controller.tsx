34571e2f4136b34bd226b23be751c0426fc2b90c
kooooichi24 <ex.kouichi1224@gmail.com>
2025-09-22 22:03:18 +0900

    feat: add SplashScreenController to manage splash screen visibility during authentication loading

---
+++ components/splash-screen-controller.tsx
@@ -0,0 +1,14 @@
+import { useAuthContext } from "@/hooks/use-auth-context";
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+
+export function SplashScreenController() {
+  const { isLoading } = useAuthContext();
+
+  if (!isLoading) {
+    SplashScreen.hideAsync();
+  }
+
+  return null;
+}
